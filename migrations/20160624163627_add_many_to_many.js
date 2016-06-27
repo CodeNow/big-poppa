a924b771e25bf4c1f88c972ef74b9caf8e91d930
thejsj <jorge.silva@thejsj.com>
2016-06-27 12:16:30 -0700

    Get rid of cascading deletes

--- migrations/20160624163627_add_many_to_many.js
+++ migrations/20160624163627_add_many_to_many.js
@@ -9,10 +9,8 @@ exports.up = function (knex, Promise) {
   let createTable = knex.schema.createTable('organization_user', function (table) {
     table.integer('organization_github_id')
       .references('organization.github_id')
-      .onDelete('CASCADE')
     table.integer('user_github_id')
       .references('user.github_id')
-      .onDelete('CASCADE')
     table.unique(['organization_github_id', 'user_github_id'])
   })
   debug(createTable.toString())
