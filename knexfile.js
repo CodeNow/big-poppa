d608bea58db5354e1631ff401d38a407edcfde4e
thejsj <jorge.silva@thejsj.com>
2016-06-30 12:08:33 -0700

    Add docker compose

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,22 @@
+'use strict'
+
 require('loadenv')() // Required for migrations
+console.log(process.env)
 
 // Update with your config settings.
+
+if (!process.env.POSTGRES_CONNECT_STRING) {
+  if (
+      process.env.POSTGRES_HOST &&
+      process.env.POSTGRES_USER &&
+      process.env.POSTGRES_DB
+  ) {
+    let host = process.env.POSTGRES_HOST
+    let user = process.env.POSTGRES_USER
+    let db = process.env.POSTGRES_DB
+    process.env.POSTGRES_CONNECT_STRING = `postgres://${user}@${host}/${db}`
+  }
+}
 
 module.exports = {
 
