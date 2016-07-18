f1369de5c290d8497c42bbe8127b33f519cb321c
thejsj <jorge.silva@thejsj.com>
2016-07-17 20:54:07 -0700

    Add unit tests for /user and /organization routes

---
+++ test/unit/http/routes/organization.js
@@ -0,0 +1,207 @@
+'use strict'
+
+const Promise = require('bluebird')
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
+const expect = require('chai').expect
+
+const NotFoundError = require('errors/not-found-error')
+const Organization = require('models/organization')
+const OrganizationRouter = require('http/routes/organization')
+
+describe('HTTP /organization', () => {
+  let collectionConstructorStub
+  let orgMock
+  let orgMockJSON
+  let responseStub
+  let fetchByIdStub
+  let requestStub
+
+  beforeEach(() => {
+    orgMockJSON = { id: 1 }
+    orgMock = {
+      save: sinon.stub().returnsThis(),
+      toJSON: sinon.stub().returns(orgMockJSON)
+    }
+    fetchByIdStub = sinon.stub(Organization, 'fetchById').resolves(orgMock)
+    responseStub = {
+      json: sinon.stub()
+    }
+  })
+
+  afterEach(() => {
+    collectionConstructorStub.restore()
+    fetchByIdStub.restore()
+  })
+
+  describe('get', () => {
+    let collectionStub
+
+    beforeEach(() => {
+      requestStub = { query: {} }
+      collectionStub = {}
+      collectionStub.query = sinon.stub().returns(collectionStub)
+      collectionStub.fetch = sinon.stub().resolves(orgMock)
+      collectionConstructorStub = sinon.stub(Organization, 'collection').returns(collectionStub)
+    })
+
+    it('should create a collection', () => {
+      return OrganizationRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(collectionConstructorStub)
+        })
+    })
+
+    it('should pass the query to `where`', () => {
+      return OrganizationRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(collectionStub.query)
+          sinon.assert.calledWithExactly(
+            collectionStub.query,
+            { where: requestStub.query }
+          )
+        })
+    })
+
+    it('should fetch the results', () => {
+      return OrganizationRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(collectionStub.fetch)
+        })
+    })
+
+    it('should pass the results to `res.json`', () => {
+      return OrganizationRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(orgMock.toJSON)
+          sinon.assert.calledOnce(responseStub.json)
+          sinon.assert.calledWithExactly(
+            responseStub.json,
+            orgMockJSON
+          )
+        })
+    })
+
+    it('should return the error if `fetch` return an error', () => {
+      let err = new Error('Sample Error')
+      collectionStub.fetch.rejects(err)
+
+      return OrganizationRouter.get(requestStub, responseStub)
+        .catch(err => {
+          expect(err).to.exist
+          expect(err).to.equal(err)
+        })
+    })
+  })
+
+  describe('getOne', () => {
+    let orgId = 7
+    beforeEach(() => {
+      requestStub = {
+        params: { id: orgId }
+      }
+    })
+
+    it('should fetch with `fetchById`', () => {
+      return OrganizationRouter.getOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(fetchByIdStub)
+          sinon.assert.calledWithExactly(
+            fetchByIdStub,
+            orgId
+          )
+        })
+    })
+
+    it('should fetch with `fetchById`', () => {
+      return OrganizationRouter.getOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(fetchByIdStub)
+          sinon.assert.calledWithExactly(
+            fetchByIdStub,
+            orgId
+          )
+        })
+    })
+
+    it('should pass the results to `res.json`', () => {
+      return OrganizationRouter.getOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(orgMock.toJSON)
+          sinon.assert.calledOnce(responseStub.json)
+          sinon.assert.calledWithExactly(
+            responseStub.json,
+            orgMockJSON
+          )
+        })
+    })
+
+    it('should return the error if `fetch` return an error', () => {
+      let err = new NotFoundError('Organization Not Found')
+      fetchByIdStub.rejects(err)
+
+      return OrganizationRouter.getOne(requestStub, responseStub)
+        .catch(err => {
+          expect(err).to.exist
+          expect(err).to.equal(err)
+        })
+    })
+  })
+
+  describe('patchOne', () => {
+    let orgId = 7
+    let stripeCustomerId = 234
+
+    beforeEach(() => {
+      requestStub = {
+        params: { id: orgId },
+        body: { stripe_customer_id: stripeCustomerId }
+      }
+    })
+
+    it('should fetch with `fetchById`', () => {
+      return OrganizationRouter.patchOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(fetchByIdStub)
+          sinon.assert.calledWithExactly(
+            fetchByIdStub,
+            orgId
+          )
+        })
+    })
+
+    it('should save the results in the body using `save`', () => {
+      return OrganizationRouter.patchOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(orgMock.save)
+          sinon.assert.calledWithExactly(
+            orgMock.save,
+            { stripe_customer_id: stripeCustomerId }
+          )
+        })
+    })
+
+    it('should pass the results to `res.json`', () => {
+      return OrganizationRouter.patchOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(orgMock.toJSON)
+          sinon.assert.calledOnce(responseStub.json)
+          sinon.assert.calledWithExactly(
+            responseStub.json,
+            orgMockJSON
+          )
+        })
+    })
+
+    it('should return the error if `fetch` return an error', () => {
+      let err = new NotFoundError('Organization Not Found')
+      fetchByIdStub.rejects(err)
+
+      return OrganizationRouter.patchOne(requestStub, responseStub)
+        .catch(err => {
+          expect(err).to.exist
+          expect(err).to.equal(err)
+        })
+    })
+  })
+})
---
+++ test/unit/http/routes/user.js
@@ -0,0 +1,150 @@
+'use strict'
+
+const Promise = require('bluebird')
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
+const expect = require('chai').expect
+
+const NotFoundError = require('errors/not-found-error')
+const User = require('models/user')
+const UserRouter = require('http/routes/user')
+
+describe('HTTP /user', () => {
+  let collectionConstructorStub
+  let userMock
+  let userMockJSON
+  let responseStub
+  let fetchByIdStub
+  let requestStub
+
+  beforeEach(() => {
+    userMockJSON = { id: 1 }
+    userMock = {
+      save: sinon.stub().returnsThis(),
+      toJSON: sinon.stub().returns(userMockJSON)
+    }
+    fetchByIdStub = sinon.stub(User, 'fetchById').resolves(userMock)
+    responseStub = {
+      json: sinon.stub()
+    }
+  })
+
+  afterEach(() => {
+    collectionConstructorStub.restore()
+    fetchByIdStub.restore()
+  })
+
+  describe('get', () => {
+    let collectionStub
+
+    beforeEach(() => {
+      requestStub = { query: {} }
+      collectionStub = {}
+      collectionStub.query = sinon.stub().returns(collectionStub)
+      collectionStub.fetch = sinon.stub().resolves(userMock)
+      collectionConstructorStub = sinon.stub(User, 'collection').returns(collectionStub)
+    })
+
+    it('should create a collection', () => {
+      return UserRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(collectionConstructorStub)
+        })
+    })
+
+    it('should pass the query to `where`', () => {
+      return UserRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(collectionStub.query)
+          sinon.assert.calledWithExactly(
+            collectionStub.query,
+            { where: requestStub.query }
+          )
+        })
+    })
+
+    it('should fetch the results', () => {
+      return UserRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(collectionStub.fetch)
+        })
+    })
+
+    it('should pass the results to `res.json`', () => {
+      return UserRouter.get(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(userMock.toJSON)
+          sinon.assert.calledOnce(responseStub.json)
+          sinon.assert.calledWithExactly(
+            responseStub.json,
+            userMockJSON
+          )
+        })
+    })
+
+    it('should return the error if `fetch` return an error', () => {
+      let err = new Error('Sample Error')
+      collectionStub.fetch.rejects(err)
+
+      return UserRouter.get(requestStub, responseStub)
+        .catch(err => {
+          expect(err).to.exist
+          expect(err).to.equal(err)
+        })
+    })
+  })
+
+  describe('getOne', () => {
+    let userId = 7
+    beforeEach(() => {
+      requestStub = {
+        params: { id: userId }
+      }
+    })
+
+    it('should fetch with `fetchById`', () => {
+      return UserRouter.getOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(fetchByIdStub)
+          sinon.assert.calledWithExactly(
+            fetchByIdStub,
+            userId
+          )
+        })
+    })
+
+    it('should fetch with `fetchById`', () => {
+      return UserRouter.getOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(fetchByIdStub)
+          sinon.assert.calledWithExactly(
+            fetchByIdStub,
+            userId
+          )
+        })
+    })
+
+    it('should pass the results to `res.json`', () => {
+      return UserRouter.getOne(requestStub, responseStub)
+        .then(() => {
+          sinon.assert.calledOnce(userMock.toJSON)
+          sinon.assert.calledOnce(responseStub.json)
+          sinon.assert.calledWithExactly(
+            responseStub.json,
+            userMockJSON
+          )
+        })
+    })
+
+    it('should return the error if `fetch` return an error', () => {
+      let err = new NotFoundError('User Not Found')
+      fetchByIdStub.rejects(err)
+
+      return UserRouter.getOne(requestStub, responseStub)
+        .catch(err => {
+          expect(err).to.exist
+          expect(err).to.equal(err)
+        })
+    })
+  })
+})
