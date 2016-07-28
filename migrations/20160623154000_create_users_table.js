88a69177c72a3ec38fd14a9695487c5d1270130c
Nathan219 <Nathan219@gmail.com>
2016-07-27 19:02:41 -0700

    remove triggers

--- migrations/20160623154000_create_users_table.js
+++ migrations/20160623154000_create_users_table.js
@@ -5,26 +5,7 @@ var debug = require('debug')('big-poppa:migration')
 /**
  * Creates the `users` table.
  */
-const CREATE_TABLE_QUERY = `
-      CREATE VIEW active_users AS
-      SELECT * FROM users WHERE is_active IS true;
-    `
-const SOFT_TRIGGER_CREATION_QUERY = `
-      CREATE FUNCTION soft_delete()
-        RETURNS trigger AS $$
-          DECLARE
-            command text := ' SET is_active = false WHERE id = $1';
-          BEGIN
-            EXECUTE 'UPDATE ' || TG_TABLE_NAME || command USING OLD.id;
-            RETURN NULL;
-          END;
-        $$ LANGUAGE plpgsql;
-    `
-const TRIGGER_ATTACH_QUERY = `
-      CREATE TRIGGER soft_delete_user
-      INSTEAD OF DELETE ON active_users
-      FOR EACH ROW EXECUTE PROCEDURE soft_delete();
-    `
+
 exports.up = function (knex, Promise) {
   var createTable = knex.schema
     .createTable('users', function (table) {
--- migrations/20160623162143_create_orgs_table.js
+++ migrations/20160623162143_create_orgs_table.js
@@ -5,15 +5,7 @@ var debug = require('debug')('big-poppa:migration')
 /**
  * Creates the `users` table.
  */
-const CREATE_TABLE_QUERY = `
-    CREATE VIEW active_organizations AS
-    SELECT * FROM organizations WHERE is_active IS true;
-  `
-const ATTACH_TRIGGER_QUERY = `
-    CREATE TRIGGER soft_delete_organization
-    INSTEAD OF DELETE ON active_organizations
-    FOR EACH ROW EXECUTE PROCEDURE soft_delete();
-  `
+
 exports.up = function (knex, Promise) {
   var createTable = knex.schema.createTable('organizations', function (table) {
     table.increments('id')
