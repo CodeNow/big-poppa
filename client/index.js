c445d21548c4a6c36fe9aa8958fa1598c6806fed
Nathan219 <Nathan219@gmail.com>
2016-07-29 13:32:39 -0700

    Adding jsDocs Switching the constructor to take a host and port use host/port env

--- client/index.js
+++ client/index.js
@@ -12,10 +12,21 @@ module.exports = class BigPoppaClient extends ApiClient {
    * @param {string} [host] Overrides the default host for the client.
    * @param {Number} [port] Overrides the default port for the client.
    */
-  constructor () {
-    super(process.env.BIG_POPPA_URL)
+  constructor (host, port) {
+    super([
+      host || process.env.BIG_POPPA_HOST,
+      port || process.env.BIG_POPPA_PORT || 80
+    ].join(':'))
   }
 
+  /**
+   * Given an internal orgId, fetch the matching org
+   *
+   * @param {String} orgId - internal Postgres Id for the organization
+   *
+   * @resolves {Organization} requested org
+   * @rejects  {Error}        when an orgId is not provided
+   */
   getOrganization (orgId) {
     if (!orgId) {
       return Promise.reject(new Error('missing orgId'))
@@ -27,6 +38,16 @@ module.exports = class BigPoppaClient extends ApiClient {
     })
   }
 
+  /**
+   * Fetches a list of orgs matching the optional parameters, or returns all of them when none are
+   * given
+   *
+   * @param {Object} opts          - optional parameters
+   * @param {String} opts.githubId - githubId of an organization
+   *
+   * @returns  {Promise}
+   * @resolves {[Organization]} requested orgs
+   */
   getOrganizations (opts) {
     var path = '/organization/'
     if (opts.githubId) {
@@ -38,6 +59,16 @@ module.exports = class BigPoppaClient extends ApiClient {
     })
   }
 
+  /**
+   * Given an internal orgId, update the matching org with the given updates
+   *
+   * @param {String} orgId - internal Postgres Id for the organization
+   * @param {Object} updates - update body
+   *
+   * @returns  {Promise}
+   * @resolves {Organization} updated requested org
+   * @rejects  {Error}        when an orgId is not provided
+   */
   updateOrganization (orgId, updates) {
     if (!orgId) {
       return Promise.reject(new Error('missing orgId'))
@@ -50,6 +81,15 @@ module.exports = class BigPoppaClient extends ApiClient {
     })
   }
 
+  /**
+   * Given an internal userId, fetch the matching user
+   *
+   * @param {String} userId - internal Postgres Id for the user
+   *
+   * @returns  {Promise}
+   * @resolves {Organization} requested user
+   * @rejects  {Error}        when an UserId is not provided
+   */
   getUser (userId) {
     if (!userId) {
       return Promise.reject(new Error('missing userId'))
@@ -61,6 +101,16 @@ module.exports = class BigPoppaClient extends ApiClient {
     })
   }
 
+  /**
+   * Fetches a list of user matching the optional parameters, or returns all of them when none are
+   * given
+   *
+   * @param {Object} opts          - optional parameters
+   * @param {String} opts.githubId - githubId of a user
+   *
+   * @returns  {Promise}
+   * @resolves {[Organization]} requested users
+   */
   getUsers (opts) {
     var path = '/user/'
     if (opts.githubId) {
