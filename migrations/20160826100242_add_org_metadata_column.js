ff074061a6b5c3065da9d3034e982a3b4c3602bd
henrymollman <hmollman@gmail.com>
2016-09-06 16:39:29 -0700

    Added new property to organization table to store JSON metadata (#25)
    * Added new property to organization table to store JSON metadata
    Specifically this will store data relevant to the aha guide/organization onboarding story
    Tests include one unit test and two functional tests that ensure the value will be a boolean
    
    * Added new method to use in extending the metadata column object, not overwriting
    Changed assertions in tests
    
    * Addressed PR comments, created new tests and updated old ones
    
    * Changes from PR feedback
    
    * Removed unnecessary parens
    
    * Changed version of postgres for travis to support JSON and fixed linting errors
    
    * Changed name of boolean to hasAha

---
+++ migrations/20160826100242_add_org_metadata_column.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var debug = require('debug')('big-poppa:migration')
+
+exports.up = function (knex, Promise) {
+  var modifyTable = knex.schema.table('organizations', function (table) {
+    table.jsonb('metadata')
+  })
+  debug(modifyTable.toString())
+  return modifyTable
+}
+
+exports.down = function (knex, Promise) {
+  var modifyTable = knex.schema.table('organizations', function (table) {
+    table.dropColumn('metadata')
+  })
+  debug(modifyTable.toString())
+  return modifyTable
+}
