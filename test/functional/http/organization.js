196277dd6bf09af54e80adaf739669560a55f4da
thejsj <jorge.silva@thejsj.com>
2016-09-07 11:19:13 -0700

    Fixing error in tests

--- test/functional/http/organization.js
+++ test/functional/http/organization.js
@@ -273,7 +273,7 @@ describe('HTTP Organization Functional Test', () => {
         })
         .asCallback(err => {
           expect(err).to.have.deep.property('data.orignalError.statusCode', 400)
-          expect(err).to.have.deep.property('data.orignalError.message', 'Validation Error')
+          expect(err.data.orignalError.message).match(/validation.*error/i)
           done()
         })
     })
