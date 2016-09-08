83cbf67dff525d399bca4bb6203f5141ffdf3feb
Nathan Meyers <nathan@runnable.com>
2016-09-07 17:15:48 -0700

    SAN 4885 - add flag for runnabot configured (#30)
    * Add flag runnabot_enabled to org table
    Add worker to enable/disable the flag
    Add tests
    Migration script
    
    * Fix wording
    Add label and required to joi schema
    
    * make the workers use transactions to update the org
    
    * Changing the name to prbot
    
    * Add tids to the worker schema
    
    * Change job to take organization.id
    
    * Fix the truncateAllTables
    
    * newline at end of file

---
+++ test/functional/workers/organization.integration.prbot.enabled.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const Promise = require('bluebird')
+const expect = require('chai').expect
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
+
+const testUtil = require('../../util')
+const githubOrganizationFixture = require('../../fixtures/github/organization')
+const githubOrgMembershipFixture = require('../../fixtures/github/org-membership')
+const githubUserFixture = require('../../fixtures/github/user')
+const MockAPI = require('mehpi')
+const githubAPI = new MockAPI(process.env.GITHUB_VARNISH_PORT)
+
+const rabbitMQ = require('util/rabbitmq')
+const Organization = require('models/organization')
+
+const PrBotEnabled = require('workers/organization.integration.prbot.enabled').task
+
+describe('Organization.integration.prbot.enabled Functional Test', () => {
+  let userGithubId = 1981198
+  let orgGithubId = 2828361
+  let publishEventStub
+  before(done => githubAPI.start(done))
+  after(done => githubAPI.stop(done))
+
+  beforeEach(() => testUtil.truncateAllTables())
+
+  beforeEach(done => {
+    let orgGithubName = githubOrganizationFixture.login.toLowerCase()
+    githubAPI.stub('GET', `/user/${userGithubId}?access_token=testing`).returns({
+      status: 200,
+      body: githubUserFixture
+    })
+    githubAPI.stub('GET', `/user/${orgGithubId}?access_token=testing`).returns({
+      status: 200,
+      body: githubOrganizationFixture
+    })
+    githubAPI.stub('GET', `/user/memberships/orgs/${orgGithubName}?access_token=testing`).returns({
+      status: 200,
+      body: githubOrgMembershipFixture
+    })
+    testUtil.createUserAndOrg(orgGithubId, userGithubId)
+      .asCallback(done)
+  })
+
+  beforeEach(() => rabbitMQ.connect())
+  afterEach(() => rabbitMQ.disconnect())
+
+  beforeEach(() => {
+    publishEventStub = sinon.stub(rabbitMQ._rabbit, 'publishEvent')
+  })
+  afterEach(() => {
+    publishEventStub.restore()
+  })
+
+  it('should enable prbot on org', done => {
+    return Organization.fetchByGithubId(orgGithubId)
+      .then(org => {
+        return PrBotEnabled({
+          organization: {
+            id: org.get(org.idAttribute)
+          }
+        })
+      })
+      .then(() => {
+        return Organization.fetchByGithubId(orgGithubId)
+      })
+      .then(org => {
+        expect(org).to.be.an('object')
+        expect(org.get('prBotEnabled')).to.equal(true)
+      })
+      .asCallback(done)
+  })
+})
---
+++ test/unit/workers/organization.integration.prbot.enabled.js
@@ -0,0 +1,116 @@
+'use strict'
+
+const Promise = require('bluebird')
+const Joi = Promise.promisifyAll(require('joi'))
+const expect = require('chai').expect
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
+
+const bookshelf = require('models').bookshelf
+const Organization = require('models/organization')
+
+const NotFoundError = require('errors/not-found-error')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
+const PrBotEnabledWorker = require('workers/organization.integration.prbot.enabled').task
+const PrBotEnabledSchema = require('workers/organization.integration.prbot.enabled').jobSchema
+
+describe('#organization.integration.prbot.enabled', () => {
+  let org
+  let orgId = 12223
+  let orgGithubId = 123
+  let validJob
+  let transaction
+
+  let fetchOrgByIdStub
+  let transactionStub
+  let orgSaveStub
+
+  beforeEach(() => {
+    org = new Organization({ id: orgId, githubId: orgGithubId })
+    validJob = { organization: { id: orgId } }
+    transaction = {}
+    transactionStub = sinon.stub(bookshelf, 'transaction', (cb) => {
+      return Promise.resolve(cb(transaction))
+    })
+    fetchOrgByIdStub = sinon.stub(Organization, 'fetchById').resolves(org)
+    orgSaveStub = sinon.stub(Organization.prototype, 'save').resolves(org)
+  })
+
+  afterEach(() => {
+    transactionStub.restore()
+    orgSaveStub.restore()
+    fetchOrgByIdStub.restore()
+  })
+
+  describe('Validation', () => {
+    it('should not validate if a `organizationId` is not passed', done => {
+      delete validJob.organization.id
+      Joi.validateAsync(validJob, PrBotEnabledSchema)
+        .asCallback(err => {
+          expect(err).to.exist
+          expect(err.message).to.match(/organization/i)
+          done()
+        })
+    })
+
+    it('should validate if a valid job is passed', done => {
+      Joi.validateAsync(validJob, PrBotEnabledSchema)
+        .asCallback(done)
+    })
+  })
+
+  describe('Errors', () => {
+    it('should throw a `WorkerStopError` if a `Organization.fetchByGithubId` throws a `NotFoundError`', done => {
+      let thrownErr = new NotFoundError('Organization not found')
+      fetchOrgByIdStub.rejects(thrownErr)
+
+      PrBotEnabledWorker(validJob)
+        .asCallback(err => {
+          expect(err).to.exist
+          expect(err).to.be.an.instanceof(WorkerStopError)
+          expect(err.data.err).to.equal(thrownErr)
+          done()
+        })
+    })
+
+    it('should throw a `WorkerStopError` if a `org.prBotEnabled` is already true', done => {
+      org.set({
+        prBotEnabled: true
+      })
+
+      PrBotEnabledWorker(validJob)
+        .asCallback(err => {
+          expect(err).to.exist
+          expect(err).to.be.an.instanceof(WorkerStopError)
+          done()
+        })
+    })
+  })
+
+  describe('Main Functionality', () => {
+    it('should fetch the organization by its id', () => {
+      return PrBotEnabledWorker(validJob)
+        .then(() => {
+          sinon.assert.calledOnce(fetchOrgByIdStub)
+          sinon.assert.calledWithExactly(
+            fetchOrgByIdStub,
+            orgId,
+            { transacting: transaction }
+          )
+        })
+    })
+
+    it('should save the prBotEnabled: true to the org', () => {
+      return PrBotEnabledWorker(validJob)
+        .then(() => {
+          sinon.assert.calledOnce(orgSaveStub)
+          let firstCall = orgSaveStub.firstCall
+          sinon.assert.calledWith(
+            firstCall,
+            { prBotEnabled: true },
+            { transacting: transaction }
+          )
+        })
+    })
+  })
+})
