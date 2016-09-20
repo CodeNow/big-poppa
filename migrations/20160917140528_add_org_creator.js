0292e0d8e5b9d5bf38c80e148c290a0f2648c132
thejsj <jorge.silva@thejsj.com>
2016-09-20 11:15:22 -0700

    Make column have no default and have it be NOT NULL

--- migrations/20160917140528_add_org_creator.js
+++ migrations/20160917140528_add_org_creator.js
@@ -5,6 +5,7 @@ let debug = require('debug')('big-poppa:migration')
 const orgUsersTableName = 'organizations_users'
 const orgTableName = 'organizations'
 const usersTableName = 'users'
+const creatorColumnName = 'creator'
 const log = require('../lib/util/logger').child({ module: 'migrations/add_org_creator' })
 
 // Get the first created user for every organization
@@ -20,10 +21,14 @@ const JOIN_QUERY = `
 // Update the creator field with a user that already belongs to an org
 const UPDATE_QUERY = `
   UPDATE ${orgTableName}
-  SET creator = org.user_id
+  SET ${creatorColumnName} = org.user_id
   FROM (
     ${JOIN_QUERY}
   ) org;
+`
+
+const ALTER_TABLE_QUERY = `
+  ALTER TABLE ${orgTableName} ALTER COLUMN ${creatorColumnName} SET NOT NULL;
 `
 
 log.info({ UPDATE_QUERY }, 'Update query')
@@ -33,9 +38,7 @@ exports.up = function (knex, Promise) {
     // Currently nullable, but will later be not-nullable after we migrate
     // all organizations
     log.trace('Add `creator` field')
-    table.integer('creator')
-      .notNullable()
-      .defaultTo(0)
+    table.integer(creatorColumnName)
   })
   .then(() => {
     log.trace('Execute raw migration')
@@ -45,9 +48,13 @@ exports.up = function (knex, Promise) {
     log.trace('Add `creator` field')
     // If there is an org with out any users, this will fail
     return knex.schema.table('organizations', function addField (table) {
-      table.foreign('creator')
+      table.foreign(creatorColumnName)
         .references('users.id')
     })
+  })
+  .then(() => {
+    // Knex doesn't have a way to modify a column
+    return knex.schema.raw(ALTER_TABLE_QUERY)
   })
   debug(modifyTable.toString())
   return modifyTable
