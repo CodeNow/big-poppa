c9830664bbbe5c03c6b1f1ad918826f7de242d1b
thejsj <jorge.silva@thejsj.com>
2016-10-06 17:24:54 -0700

    Add subscription id to fields

--- migrations/20161006154914_add_stripe_subscription_field.js
+++ migrations/20161006154914_add_stripe_subscription_field.js
@@ -10,29 +10,28 @@ const NEW_PROPERTY = 'stripe_subscription_id'
 exports.up = function (knex, Promise) {
   var modifyTable = knex.schema.table(TABLE_NAME, function addField (table) {
     log.trace('Add `stripe_subscription_id` field')
-    table.string(NEW_PROPERTY)
+    table.string(NEW_PROPERTY).unique()
   })
   .then(function () {
     // Add subscription IDs to all organizations
     return knex.select('*').from(TABLE_NAME).whereNotNull('stripe_customer_id')
   })
   .then(function (organizations) {
-    console.log(organizations.length)
     return Promise.all(organizations.map(function (org) {
-      console.log('1')
-      return stripe.customer.retrieve(org.stripe_customer_id)
+      return stripe.customers.retrieve(org.stripe_customer_id)
       .then(function (stripeCustomer) {
-        console.log(stripeCustomer)
-        // return knex(TABLE_NAME)
-        // .where('id', org.id)
-        // .update({
-          // stripe_subscription_id:
-        // })
+        var subscriptions = stripeCustomer.subscriptions.data
+        if (subscriptions.length > 1) {
+          throw new Error('Ambigious amount of subscriptions for customer (more than 1)')
+        }
+        let updates = {}
+        updates[NEW_PROPERTY] = subscriptions[0].id
+        log.trace({ updates, org }, 'Updating organization')
+        return knex(TABLE_NAME)
+        .where('id', org.id)
+        .update(updates)
       })
     }))
-  })
-  .then(function () {
-    throw new Error('!! ROLLBACK !!')
   })
   log.trace(modifyTable.toString())
   return modifyTable
