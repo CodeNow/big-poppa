c0dde7860b87c6d12bdef86ed77ced9569e69de7
thejsj <jorge.silva@thejsj.com>
2016-10-18 14:01:06 -0700

    Re-adding deleted migration part

--- migrations/20161006154914_add_stripe_subscription_field.js
+++ migrations/20161006154914_add_stripe_subscription_field.js
@@ -1,6 +1,7 @@
 'use strict'
 
 require('loadenv')()
+const stripe = require('stripe')(process.env.STRIPE_API_KEY)
 const log = require('../lib/util/logger').child({ module: 'migrations/add_stripe_subscription_field' })
 
 const TABLE_NAME = 'organizations'
@@ -10,6 +11,27 @@ exports.up = function (knex, Promise) {
   var modifyTable = knex.schema.table(TABLE_NAME, function addField (table) {
     log.trace('Add `stripe_subscription_id` field')
     table.string(NEW_PROPERTY).unique()
+  })
+  .then(function () {
+    // Add subscription IDs to all organizations
+    return knex.select('*').from(TABLE_NAME).whereNotNull('stripe_customer_id')
+  })
+  .then(function (organizations) {
+    return Promise.all(organizations.map(function (org) {
+      return stripe.customers.retrieve(org.stripe_customer_id)
+      .then(function (stripeCustomer) {
+        var subscriptions = stripeCustomer.subscriptions.data
+        if (subscriptions.length > 1) {
+          throw new Error('Ambigious amount of subscriptions for customer (more than 1)')
+        }
+        let updates = {}
+        updates[NEW_PROPERTY] = subscriptions[0].id
+        log.trace({ updates, org }, 'Updating organization')
+        return knex(TABLE_NAME)
+        .where('id', org.id)
+        .update(updates)
+      })
+    }))
   })
   log.trace(modifyTable.toString())
   return modifyTable
