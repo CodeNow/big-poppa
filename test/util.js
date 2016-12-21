c12896fc55ad72c95aeeebdf3b9ffcd78270f215
thejsj <jorge.silva@thejsj.com>
2016-12-21 15:36:11 -0800

    Change deletion order

--- test/util.js
+++ test/util.js
@@ -13,13 +13,8 @@ module.exports = class TestUtil {
 
   static truncateAllTables () {
     return knex('organizations_users').truncate()
-      .then(() => {
-        return Promise.all([
-          // Cannot truncate because of foreign key constraint
-          knex('organizations').del(),
-          knex('users').del()
-        ])
-      })
+      .then(() => knex('organizations').del())
+      .then(() => knex('users').del())
   }
 
   static createUser (userGithubId, token) {
