0e2248cc81f3ee0c3e102da5bdaf57338b0a2d5b
Jorge Silva <jorge.silva@thejsj.com>
2017-02-02 12:51:47 -0800

    Bear clone (#67)
    ### What this PR does
    
    * Adds pool to knexfile (throws error if it's not declared)
    * Adds job to publisher to ensure queue gets created
    
    ### Tests
    
    - [x] Delete the queue and deploy to gamma 
    
    ### Commits
    
    * Add pool
    * Make sure queue exists

--- knexfile.js
+++ knexfile.js
@@ -3,45 +3,13 @@ require('loadenv')() // Required for migrations
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: process.env.POSTGRES_CONNECT_STRING,
-    migrations: {
-      tableName: 'migrations'
-    }
+  client: 'pg',
+  connection: process.env.POSTGRES_CONNECT_STRING,
+  migrations: {
+    tableName: 'migrations'
   },
-
-  test: {
-    client: 'pg',
-    connection: process.env.POSTGRES_CONNECT_STRING,
-    migrations: {
-      tableName: 'migrations'
-    }
-  },
-
-  'production-gamma': {
-    client: 'pg',
-    connection: process.env.POSTGRES_CONNECT_STRING,
-    migrations: {
-      tableName: 'migrations'
-    }
-  },
-
-  'production-epsilon': {
-    client: 'pg',
-    connection: process.env.POSTGRES_CONNECT_STRING,
-    migrations: {
-      tableName: 'migrations'
-    }
-  },
-
-  'production-delta': {
-    client: 'pg',
-    connection: process.env.POSTGRES_CONNECT_STRING,
-    migrations: {
-      tableName: 'migrations'
-    }
+  pool: {
+    min: process.env.POSTGRES_POOL_MIN,
+    max: process.env.POSTGRES_POOL_MAX
   }
-
 }
