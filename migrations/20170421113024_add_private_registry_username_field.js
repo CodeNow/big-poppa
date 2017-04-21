7004878f4c68d276f7c158f6fd0d7ca06a827bfa
Myztiq <kahn@runnable.com>
2017-04-21 12:59:26 -0700

    we can now save private registry url and username.

---
+++ migrations/20170421113024_add_private_registry_username_field.js
@@ -0,0 +1,24 @@
+'use strict'
+
+require('loadenv')()
+const log = require('../lib/util/logger').child({ module: 'migrations/add_private_registry' })
+
+const TABLE_NAME = 'organizations'
+const NEW_PROPERTY = 'privateRegistryUsername'
+
+exports.up = function (knex, Promise) {
+  var modifyTable = knex.schema.table(TABLE_NAME, function addField (table) {
+    log.trace(`Add '${NEW_PROPERTY}' field`)
+    table.string(NEW_PROPERTY)
+  })
+  log.trace(modifyTable.toString())
+  return modifyTable
+}
+
+exports.down = function (knex, Promise) {
+  var modifyTable = knex.schema.table(TABLE_NAME, function (table) {
+    table.dropColumn(NEW_PROPERTY)
+  })
+  log.trace(modifyTable.toString())
+  return modifyTable
+}
